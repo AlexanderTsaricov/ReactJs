c3cdfcd7b8856cf129149ddca5642afe5e83a472
Alexandr Vinokurov <salispiligrim@yandex.ru>
2025-02-23 13:40:10 +0500

    create product page and add to product page header, bread crumps

--- attestation/src/App.js
+++ attestation/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Home from "./pages/Home";
 import Catalog from "./pages/Catalog";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/catalog" element={<Catalog />} />
+                <Route path="/product" element={<Product />} />
             </Routes>
         </Router>
     );
---
+++ attestation/src/pages/Product.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/headers/Header";
+import ToggleMenu from "../components/ToggleMenu";
+import BreadCrumpsBox from "../components/BeadCrumpsBox";
+
+function Product() {
+    return (
+        <>
+            <Header />
+            <ToggleMenu />
+            <BreadCrumpsBox />
+        </>
+    );
+}
+
+export default Product;
