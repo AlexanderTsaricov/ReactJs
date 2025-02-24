5068724fd3debc0afaddf804dcfe2c8c07977e27
Alexandr Vinokurov <salispiligrim@yandex.ru>
2025-02-24 08:16:28 +0500

    create add product action in reducer

--- attestation/src/productsData/brandReduser.js
+++ attestation/src/productsData/brandReduser.js
@@ -1,9 +1,27 @@
+import { ADD_PRODUCT } from "./actions/productActions";
+
 const initialState = {
     cart: [],
 };
 
 const brandReducer = (state = initialState, action) => {
     switch (action.type) {
+        case ADD_PRODUCT:
+            const id = action.payload.id;
+            const isHaveProduct = state.cart.some((product) => product.id === id);
+            if (isHaveProduct) {
+                return {
+                    ...state,
+                    cart: state.cart.map((product) =>
+                        product.id === id ? { ...product, count: product.count + 1 } : product
+                    ),
+                };
+            } else {
+                return {
+                    ...state,
+                    cart: [...state.cart, { count: 1, ...action.payload }],
+                };
+            }
         default:
             return state;
     }
